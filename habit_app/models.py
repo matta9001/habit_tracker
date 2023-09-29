d5278f97e91650f884a4bdc7e8fab80c83db2ff7
Matt Anderson <matta9001@gmail.com>
2023-09-29 12:05:07 -0500

    Add data structure for UTC timestamps of checkin
    Green border for >= 48H
    Yellow for >= 24H
    Red for >= 0H
    Nothing for less

--- habit_app/models.py
+++ habit_app/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import time
+
 def get_default_empty_list():
-    return []
+    utc_time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
+    return [ utc_time_str ]
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='profile_pictures/', default='profile.png')
     habits = models.CharField(max_length=200)
     checkins = models.JSONField(default=get_default_empty_list)
-    active = models.BooleanField(default=False)
--- habit_app/views.py
+++ habit_app/views.py
@@ -5,14 +5,44 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import EditUserForm
 from .models import UserProfile
 
-# Create your views here.
+import time
+from datetime import datetime
+
+def hours_since_time(utc_time_str):
+    input_time = datetime.strptime(utc_time_str, '%Y-%m-%d %H:%M:%S')
+    current_time = datetime.utcfromtimestamp(time.time())
+    time_difference = current_time - input_time
+    hours_difference = time_difference.total_seconds() // 3600
+    return int(hours_difference)
+
 def index(request):
     return render(request, 'index.html')
 
 @login_required
 def profile(request):
     user_profile, _ = UserProfile.objects.get_or_create(user=request.user)
-    return render(request, 'profile.html')
+
+    last_checkin = user_profile.checkins[-1]
+    hours_since = hours_since_time(last_checkin)
+    checkin_window = min(48 - hours_since, 48)
+
+    context = {}
+
+    if checkin_window >= 48:
+        context['border_color'] = 'border-success'
+    elif checkin_window >= 24:
+        context['border_color'] = 'border-warning'
+    elif checkin_window >= 1:
+        context['border_color'] = 'border-danger'
+    elif checkin_window >= 1:
+        context['border_color'] = ''
+
+    if checkin_window >= 48:
+        context['window_message'] = f"Good to go!"
+    elif checkin_window > 0:
+        context['window_message'] = f"You have {checkin_window} hours to check in."
+
+    return render(request, 'profile.html', context)
 
 @login_required
 def manage(request):
