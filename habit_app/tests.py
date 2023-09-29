bbbde2c5b355bf2ec47b30e7f86706717881ea27
Matt Anderson <matta9001@gmail.com>
2023-09-29 13:48:21 -0500

    Calculate streak when loading profile, with tests!

--- habit_app/tests.py
+++ habit_app/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from habit_app.views import hours_since_time, compare_utc, calculate_streak
+import time
+from datetime import datetime
 
 # Create your tests here.
+class TestTimeFunctions(TestCase):
+
+    def test_hours_since_time(self):
+        # Mocking current time to a fixed value for testing
+        fixed_time = datetime(2023, 9, 28, 14, 17, 0)
+        time.time = lambda: fixed_time.timestamp()
+        self.assertEqual(hours_since_time('2023-09-28 13:17:00'), 1)
+        self.assertEqual(hours_since_time('2023-09-27 14:17:00'), 24)
+        self.assertEqual(hours_since_time('2023-09-26 14:17:00'), 48)
+
+    def test_compare_utc(self):
+        self.assertEqual(compare_utc('2023-09-28 13:17:00', '2023-09-28 14:17:00'), 1)
+        self.assertEqual(compare_utc('2023-09-27 14:17:00', '2023-09-28 14:17:00'), 24)
+        self.assertEqual(compare_utc('2023-09-26 14:17:00', '2023-09-28 14:17:00'), 48)
+
+    def test_calculate_streak(self):
+        utc_list = ["2023-09-20 13:17:00", "2023-09-21 13:17:00", "2023-09-22 13:17:00", "2023-09-23 13:17:00", "2023-09-24 13:17:00"]
+        self.assertEqual(calculate_streak(utc_list), 96)  # Assuming the output is in hours
+        # Additional test cases
+        utc_list_2 = ["2023-09-22 13:17:00"]
+        self.assertEqual(calculate_streak(utc_list_2), 0)  # Edge case with a single date
+        utc_list_3 = []  # Edge case with no dates
+        self.assertEqual(calculate_streak(utc_list_3), 0)
+
+        # Additional test cases with varying date intervals
+        utc_list_4 = ["2023-09-22 13:17:00", "2023-09-23 14:17:00", "2023-09-24 15:17:00",
+                      "2023-09-25 13:17:00", "2023-09-26 13:17:00"]
+        self.assertEqual(calculate_streak(utc_list_4), 96)
--- habit_app/views.py
+++ habit_app/views.py
@@ -8,25 +8,59 @@ from .models import UserProfile
 import time
 from datetime import datetime
 
+# UTC Format: '2023-09-28 14:17:00'
+# Calculates the distance in hours from the input and the current time
 def hours_since_time(utc_time_str):
     input_time = datetime.strptime(utc_time_str, '%Y-%m-%d %H:%M:%S')
     current_time = datetime.utcfromtimestamp(time.time())
     time_difference = current_time - input_time
-    hours_difference = time_difference.total_seconds() // 3600
-    return int(hours_difference)
+    hours_difference = time_difference.total_seconds() / 3600
+
+    return round(hours_difference)
+
+
+# Calculates the distance in hours from the two inputs
+def compare_utc(utc_time_str_past, utc_time_str_future):
+    past = datetime.strptime(utc_time_str_past, '%Y-%m-%d %H:%M:%S')
+    future = datetime.strptime(utc_time_str_future, '%Y-%m-%d %H:%M:%S')
+    time_difference = future - past
+    hours_difference = time_difference.total_seconds() / 3600
+
+    return round(hours_difference)
+
+
+# Function that finds the longest contiguous streak from the most recent checkin
+# This is defined as a smaller than 48 hour window between each sequential checkin
+def calculate_streak(utc_list):
+    if len(utc_list) <= 1:
+        return 0
+
+    streak_index = 0
+    utc_list_reversed = utc_list[::-1]
+    for i in range(len(utc_list_reversed) - 1):
+        hours_difference = compare_utc(utc_list_reversed[i+1], utc_list_reversed[i])
+        if hours_difference > 0 and hours_difference <= 48:
+            streak_index = i+1
+        else:
+            break
+    
+    return compare_utc(utc_list_reversed[streak_index], utc_list_reversed[0])
+
 
 def index(request):
     return render(request, 'index.html')
+
 
 @login_required
 def profile(request):
     user_profile, _ = UserProfile.objects.get_or_create(user=request.user)
 
+    context = {}
+
+    # checkin Availability
     last_checkin = user_profile.checkins[-1]
     hours_since = hours_since_time(last_checkin)
     checkin_window = min(48 - hours_since, 48)
-
-    context = {}
 
     if checkin_window >= 48:
         context['border_color'] = 'border-success'
@@ -36,13 +70,19 @@ def profile(request):
         context['border_color'] = 'border-danger'
     elif checkin_window >= 1:
         context['border_color'] = ''
-
     if checkin_window >= 48:
-        context['window_message'] = f"Good to go!"
+        context['window_message'] = f"Great work!"
     elif checkin_window > 0:
         context['window_message'] = f"You have {checkin_window} hours to check in."
 
+    #print(calculate_streak(["2023-09-28 18:17:00", "2023-09-29 18:17:00"]))
+
+    # Calculate Streak 
+    streak = round(calculate_streak(user_profile.checkins) / 24)
+    context['streak'] = streak
+
     return render(request, 'profile.html', context)
+
 
 @login_required
 def manage(request):
